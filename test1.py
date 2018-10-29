6127be700a112fa5fcf9b1664c6ae911dc8c5553
mayaton <25860450+mayaton@users.noreply.github.com>
2018-10-29 14:12:52 +0000

    IF文を関数にして外出し

--- test1.py
+++ test1.py
@@ -9,27 +9,26 @@ def main():
 
 
     #skd_dataとoneの読み込み------------------------------------
-    sdk_data_path = os.path.dirname(__file__)+"\\ktk.bin"
+    sdk_data_path = os.path.dirname(__file__)+"/ktk.bin"
 
     with open(sdk_data_path, 'rb') as base_file:
         cards  = pickle.load(base_file)
 
     #必要な情報を配列に入れ直す
     #汚い…
+    
 
     ex = []
-    for c in cards:
-        try:
-            for f in c.foreign_names:
-                if f["language"] == "Japanese":
-                    jpid = f["multiverseid"]
-        except:
-            jpid = 0
-        ex.append({"name":c.name,"multiverse_id":c.multiverse_id,"jp_multiverse_id":jpid})
+    for card in cards:
+        for foreign_name in card.foreign_names:
+            jpid = get_jpid(foreign_name)
+            ex.append({"name":card.name,"multiverse_id":card.multiverse_id,"jp_multiverse_id":jpid})
+
+    print(ex)
 
     #one.json読み込み----------------------------------
 
-    one_path = os.path.dirname(__file__)+"\\one.json"
+    one_path = os.path.dirname(__file__)+"/one.json"
     card_json = open(one_path, 'r',encoding="utf-8")
     card_j= json.load(card_json)
     card_json.close()
@@ -48,11 +47,17 @@ def main():
             pass
 
     #データを保存-----------------------------------------------------------------------------------
-    output_path = os.path.dirname(__file__) + "\\output.json"
+    output_path = os.path.dirname(__file__) + "/output.json"
     fw = open(output_path,'w',encoding='utf-8')
     json.dump(card_j,fw,indent=4,ensure_ascii=False)
     fw.close()
 
+def get_jpid(foreign_name):
+# 日本語見つけたらmultiverseid返すよ関数
+    if foreign_name["language"] == "Japanese":
+        return foreign_name["multiverseid"]
+    else:
+        return 0
 
 if __name__=='__main__':
     main()
