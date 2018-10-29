6ad70e0da4821473cfd8978eda22d0c87f4b52d0
May <sh.vr.m0083@gmail.com>
2018-10-29 21:52:49 +0900

    名前変え忘れてたので

--- test1.py
+++ test1.py
@@ -9,7 +9,7 @@ def main():
 
 
     #skd_dataとoneの読み込み------------------------------------
-    sdk_data_path = os.path.dirname(__file__)+"\\sdk_data.bin"
+    sdk_data_path = os.path.dirname(__file__)+"\\ktk.bin"
 
     with open(sdk_data_path, 'rb') as base_file:
         cards  = pickle.load(base_file)
@@ -38,12 +38,14 @@ def main():
     #名前で検索してid登録～～～----------------------------------
 
     for x in card_j:
-
-        filter_result = filter(lambda x:x["name"] == "Thalia, Guardian of Thraben",ex)
-        res = list(filter_result)[-1]
-        card_j[x]["multiverseid"] = res["multiverse_id"]
-        card_j[x]["jp_multiverseid"] = res["jp_multiverse_id"]
-        
+        name = card_j[x]["name"]
+        filter_result = filter(lambda x:x["name"] == name,ex)
+        try:
+            res = list(filter_result)[-1]
+            card_j[x]["multiverseid"] = res["multiverse_id"]
+            card_j[x]["jp_multiverseid"] = res["jp_multiverse_id"]
+        except:
+            pass
 
     #データを保存-----------------------------------------------------------------------------------
     output_path = os.path.dirname(__file__) + "\\output.json"
