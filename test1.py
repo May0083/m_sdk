687ad315aa50f21b79f2151d4cb5ccc90bb06515
May <sh.vr.m0083@gmail.com>
2018-10-30 21:46:46 +0900

    スマートな気配

--- test1.py
+++ test1.py
@@ -4,43 +4,56 @@ import os
 import pickle
 
 def main():
-    ex = read_bin()
+    cards = read_sdk()
     card_j = read_json()
 
     #名前で検索してid登録～～～----------------------------------
 
     for x in card_j:
         name = card_j[x]["name"]
-        filter_result = filter(lambda x:x["name"] == name,ex)
-        #これをしたいがためにリストに作り直した
-        #cards = Card.all()
-        #filter_result = filter(lambda x:x["name"] == name,cards)
-        #↑こんな感じに出来ればよかったのに・・・
-
-        try:
-            res = list(filter_result)[-1]
-            card_j[x]["multiverseid"] = res["multiverseid"]
-            card_j[x]["jp_multiverseid"] = res["jp_multiverseid"]
-        except:
-            pass
-
+        card_j[x]["multiverseid"] = get_multiverseid(name,cards)
+        card_j[x]["jp_multiverseid"] = get_multiverseid_jp(name,cards)
     save_data(card_j)
 
-def read_bin():
+def get_multiverseid(name,cards):
+    id_list = []
+    for card in cards:
+        if card.name == name:
+            if card.multiverse_id != None: id_list.append(card.multiverse_id)
+    id_list.sort()
+    return id_list[0]
+
+def get_multiverseid_jp(name,cards):
+    id_list = []
+    for card in cards:
+        if card.name == name:
+            jpid = get_jpid(card)
+            if card.multiverse_id != 0: id_list.append(jpid)
+    id_list.sort()
+    return id_list[0]
+
+
+def get_jpid(card):
+# 日本語見つけたらmultiverseid返すよ関数
+    jpid = 0
+    try: #他のエキスパンションだとcard.foreign_namesが存在しない場合があり、エラーが生じる。
+        for foreign_name in card.foreign_names:
+            if foreign_name["language"] == "Japanese":#この後に他の言語をチェックするとidが0に上書きされてしまう問題
+                jpid = foreign_name["multiverseid"]
+    except:
+        pass
+    
+    return jpid
+    
+def read_sdk():
+    #本番はこっち
+    #cards = Card.all()
     sdk_data_path = os.path.dirname(__file__)+"/ktk.bin"
 
     with open(sdk_data_path, 'rb') as base_file:
         cards  = pickle.load(base_file)
 
-    ex = []
-    for card in cards:
-        jpid = get_jpid(card)
-        jptext = get_jptext(card)
-        ex.append({"name":card.name,"multiverseid":card.multiverse_id,"jp_multiverseid":jpid})
-
-    print(ex)
-    return ex
-
+    return cards
 
 def read_json():
     one_path = os.path.dirname(__file__)+"/one.json"
@@ -55,17 +68,6 @@ def save_data(card_j):
     json.dump(card_j,f,indent=4,ensure_ascii=False)
     f.close()
 
-def get_jpid(card):
-# 日本語見つけたらmultiverseid返すよ関数
-    jpid = 0
-    try: #他のエキスパンションだとcard.foreign_namesが存在しない場合があり、エラーが生じる。
-        for foreign_name in card.foreign_names:
-            if foreign_name["language"] == "Japanese":#これがないとこの後に他の言語をチェックするとidが0に上書きされてしまう問題
-                jpid = foreign_name["multiverseid"]
-    except:
-        pass
-    
-    return jpid
 
 
 if __name__=='__main__':
