5ca788e3e5f93c8288a5b2304ea67d449c2b1250
May <sh.vr.m0083@gmail.com>
2018-10-31 12:33:42 +0900

    no message

--- test1.py
+++ test1.py
@@ -4,37 +4,43 @@ import os
 import pickle
 
 def main():
-    cards = read_sdk()
+  
+    cards = Card.where(set="AVR").all()
     card_j = read_json()
 
-    #名前で検索してid登録～～～----------------------------------
 
     for x in card_j:
         name = card_j[x]["name"]
         card_j[x]["multiverseid"] = get_multiverseid(name,cards)
         card_j[x]["jp_multiverseid"] = get_multiverseid_jp(name,cards)
+
     save_data(card_j)
 
 def get_multiverseid(name,cards):
-    id_list = []
-    for card in cards:
-        if card.name == name:
-            if card.multiverse_id != None: id_list.append(card.multiverse_id)
-    id_list.sort()
-    return id_list[0]
+    try:#たまに無いこともある
+        id_list = []
+        for card in cards:
+            if card.name == name:
+                if card.multiverse_id != None: id_list.append(card.multiverse_id) 
+        id_list.sort()
+        return id_list[0]
+    except:
+        return 0
 
 def get_multiverseid_jp(name,cards):
-    id_list = []
-    for card in cards:
-        if card.name == name:
-            jpid = get_jpid(card)
-            if card.multiverse_id != 0: id_list.append(jpid)
-    id_list.sort()
-    return id_list[0]
-
+    try:#無いことがよくある
+        id_list = []
+        for card in cards:
+            if card.name == name:
+                jpid = get_jpid(card)
+                if jpid != 0: id_list.append(jpid)
+        id_list.sort()
+        return id_list[0]
+    except:
+        return 0
 
 def get_jpid(card):
-# 日本語見つけたらmultiverseid返すよ関数
+    # 日本語見つけたらmultiverseid返すよ関数
     jpid = 0
     try: #他のエキスパンションだとcard.foreign_namesが存在しない場合があり、エラーが生じる。
         for foreign_name in card.foreign_names:
@@ -45,18 +51,8 @@ def get_jpid(card):
     
     return jpid
     
-def read_sdk():
-    #本番はこっち
-    #cards = Card.all()
-    sdk_data_path = os.path.dirname(__file__)+"/ktk.bin"
-
-    with open(sdk_data_path, 'rb') as base_file:
-        cards  = pickle.load(base_file)
-
-    return cards
-
 def read_json():
-    one_path = os.path.dirname(__file__)+"/one.json"
+    one_path = os.path.dirname(__file__)+"/little.json"
     card_json = open(one_path, 'r',encoding="utf-8")
     card_j= json.load(card_json)
     card_json.close()
